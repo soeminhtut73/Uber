a6c7a7fc4a25d5056b7cad45e7c5aafd6e2948c5
smh <soeminhtut73@gmail.com>
2024-04-08 01:23:06 +0800

    loginUI setup

--- Uber Clone/Controllers/LoginViewController.swift
+++ Uber Clone/Controllers/LoginViewController.swift
@@ -12,16 +12,99 @@ class LoginViewController: UIViewController {
         
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let view = UIView().inputContainerView(image: "ic_mail_outline_white_2x", textField: emailTextField)
+        
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContainerView(image: "ic_lock_outline_white_2x", textField: passwordTextField)
+        
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField().textField(withPlaceholder: "Email", isSecureTextEntry: false)
+        
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField().textField(withPlaceholder: "Password", isSecureTextEntry: true)
+        
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(UIColor.label, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.backgroundColor = .mainBlueTint
+        button.layer.cornerRadius = 5
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return button
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        let title = NSMutableAttributedString(string: "Don't Have An Account? ", attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor : UIColor.lightGray
+        ])
+        
+        let signUpTitle = NSAttributedString(string: " SignUp.", attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor : UIColor.mainBlueTint
+        ])
+        
+        title.append(signUpTitle)
+        button.setAttributedTitle(title, for: .normal)
+        
+        return button
+    }()
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        view.backgroundColor = UIColor.backgroundColor
         
+        /// title lable layout
         view.addSubview(titleLable)
+        titleLable.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLable.centerX(inView: view)
         
-        titleLable.translatesAutoresizingMaskIntoConstraints = false
-        titleLable.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        /// arrange with stackview
+        let stackView = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, loginButton])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 24
+        view.addSubview(stackView)
+        stackView.anchor(top: titleLable.bottomAnchor,
+                         left: view.leftAnchor,
+                         right: view.rightAnchor,
+                         paddingTop: 40,
+                         paddingLeft: 16,
+                         paddingRight: 16)
+        
+        /// signup button layout
+        view.addSubview(signUpButton)
+        signUpButton.centerX(inView: view)
+        signUpButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor,paddingBottom: 30)
+        
+        
+        /*
+        /// email container layout
+        view.addSubview(emailContainerView)
+        emailContainerView.anchor(top: titleLable.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 15, paddingRight: 15, height: 50)
+        
+        /// password container layout
+        view.addSubview(passwordContainerView)
+        passwordContainerView.anchor(top: emailTextField.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 15, paddingRight: 15, height: 50)
+         */
+        
+        
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
--- Uber Clone/Utils/Extension.swift
+++ Uber Clone/Utils/Extension.swift
@@ -1,7 +1,103 @@
 import UIKit
 
+//MARK: - UIColor Extension
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor.init(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
+    }
+    
+    static var backgroundColor = UIColor.rgb(red: 25, green: 25, blue: 25)
+    static var mainBlueTint = UIColor.rgb(red: 17, green: 154, blue: 237)
+}
+
+//MARK: - UIView Extension
 extension UIView {
-    func anchor() {
+    
+    func inputContainerView(image: String, textField: UITextField) -> UIView {
+        let view = UIView()
+        view.anchor(height: 50)
+        
+        let imageView = UIImageView()
+        view.addSubview(imageView)
+        imageView.image = UIImage(imageLiteralResourceName: image)
+        imageView.alpha = 0.8
+        imageView.centerY(inView: view)
+        imageView.anchor(left: view.leftAnchor, paddingLeft: 10, width: 26, height: 26)
+        
+        view.addSubview(textField)
+        textField.centerY(inView: view)
+        textField.anchor(left: imageView.rightAnchor, right: view.rightAnchor, paddingLeft: 6, paddingRight: 6, height: 26)
+        
+        let separatorView = UIView()
+        view.addSubview(separatorView)
+        separatorView.backgroundColor = .lightGray
+        separatorView.anchor(left: view.leftAnchor, right: view.rightAnchor, bottom: view.bottomAnchor, paddingLeft: 10, height: 0.75)
+        
+        return view
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+    }
+    
+    func centerX(inView view: UIView) {
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func centerY(inView view: UIView) {
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+}
+
+//MARK: - UITextField Extension
+extension UITextField {
+    
+    func textField(withPlaceholder placeholder: String, isSecureTextEntry: Bool) -> UITextField {
+        let textField = UITextField()
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.borderStyle = .none
+        textField.isSecureTextEntry = isSecureTextEntry
+        textField.textColor = UIColor.label
+        textField.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor : UIColor.secondaryLabel])
+        return textField
         
     }
 }
+
